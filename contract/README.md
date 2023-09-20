9fcba98d5c948e5ede98120022d9b66d4fbef785
linziyang <lzyoutlook@gmail.com>
2023-09-20 20:08:41 +0800

    verify contract with arguments

--- contract/README.md
+++ contract/README.md
@@ -6,3 +6,8 @@
 2. 0xf051E0f27188907e61A8b78d61946f22240A9658
 3. 0xe2507d1E8634A83027E9975B08525b49F14DA2FD [2023/08/27]
 4. 0x6BbcceCe69C3D496DfF9fB7F065822e1e6207d93
+
+
+## verify contract with args
+
+https://hardhat.org/hardhat-runner/plugins/nomicfoundation-hardhat-verify
--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+// import { ProxyAgent, setGlobalDispatcher } from "undici"
+const ProxyAgent = require("undici").ProxyAgent
+const setGlobalDispatcher = require("undici").setGlobalDispatcher
+const proxyAgent = new ProxyAgent('http://127.0.0.1:7890'); // change to yours
+setGlobalDispatcher(proxyAgent);
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: `http endpoint`,
+      accounts: ["privatekey"],
+    },
+  },
+  etherscan: {
+    apiKey: "",
+  },
 };
